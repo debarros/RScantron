b0d7e878807bd07b769ad08e464802c48a35b3f1
pj.debarros@gmail.com <pj.debarros@gmail.com>
2015-05-08 08:31:11 -0400

    Fixed name of returned variable in FindRecentEvents.
    Made FindRecentTests sort the results descending by the number of instances of that test in RecentEventFrame.

--- FindRecent.R
+++ FindRecent.R
@@ -2,16 +2,20 @@
 
 FindRecentEvents = function(EventFrame, RecentDays){
   RecentEventFrame = EventFrame[which(EventFrame$Date >= Sys.Date()-(RecentDays - 1)),]
-  return(x)
+  return(RecentEventFrame)
 }
-
 
 
 #FindRecentTests function
 FindRecentTests = function(RecentEventFrame){
   RecentTestFrame = data.frame(Published.Test = unique(RecentEventFrame$Published.Test))
+  RecentTestFrame$Date = as.Date(NA)
   for (i in 1:nrow(RecentTestFrame)){
     RecentTestFrame$Count[i] = sum(RecentEventFrame$Published.Test == RecentTestFrame$Published.Test[i])
+    RecentTestFrame$Date[i] = as.Date(min(
+      RecentEventFrame$Date[which(
+        RecentEventFrame$Published.Test == RecentTestFrame$Published.Test[i])]))
   }
+  RecentTestFrame = RecentTestFrame[order(-RecentTestFrame$Count), ]
   return(RecentTestFrame)
 }
