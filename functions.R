9f90c97814267cdc6741f8f7abcc2ae1a9ab52d4
pj.debarros@gmail.com <pj.debarros@gmail.com>
2015-10-14 12:36:55 -0400

    In functions.R file, added source statements for a few new files.  Also added code to read in the TeacherLoookup and CourseLookup tables.

--- functions.R
+++ functions.R
@@ -1,5 +1,14 @@
-# functions
+# functions.R
 
+
+#libraries
+library(XML)
+library(RCurl)
+library(stringr)
+library(reshape2)
+
+
+#functions
 source("FindDrafts.R")
 source("login.R")
 source("FindEvents.R")
@@ -8,3 +17,8 @@ source("FindStudents.R")
 source("FindTests.R")
 source("FindRecent.R")
 source("logout.R")
+source("getToken.R")
+source("getOrgID.R")
+source("FindEvents_1Student.R")
+TeacherLookup = read.csv(file = "TeacherLookup.csv", stringsAsFactors = FALSE)
+CourseLookup = read.csv(file = "CourseLookup.csv", stringsAsFactors = FALSE)
