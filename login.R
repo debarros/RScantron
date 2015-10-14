a91f8621c34c43c74ad4b2e806f7f9abc7539973
pj.debarros@gmail.com <pj.debarros@gmail.com>
2015-10-14 12:35:05 -0400

    Removed library calls from login() function.  Added the new 2-step login URL's.  Created process for 2-step login.

--- login.R
+++ login.R
@@ -3,13 +3,9 @@ login = function(username = character(),
                  SiteCode = character(), 
                  caLocation = character()){
 
-  library(XML)
-  library(RCurl)
-  library(stringr)
-  library(reshape2)
-  
   #set the address for the achievement series login page
-  loginurl = "https://admin.achievementseries.com/!/login.ssp"  
+  loginurl1 = "https://admin.achievementseries.com/Auth/Login/Org"  
+  loginurl2 = "https://admin.achievementseries.com/Auth/?returnUrl=%2FAuth%2FLogin%2FUser"
   
   #if the location of the certificate file is not set, download the file
   if(length(caLocation) == 0){
@@ -26,14 +22,7 @@ login = function(username = character(),
   if(length(username) == 0){username = readline(prompt="Staff ID: ")}  
   if(length(password) == 0){password = readline(prompt="password: ")}
   
-  #Set user account data and agent
-  pars=list(
-    "JavaScriptTest" = "yes",
-    "username"= username,
-    "password"= password,
-    "SiteCode" = SiteCode,
-    "_PageAction" = "o"
-  )
+  #Set the "agent" (the info that tells web servers what browsers we are using)
   agent="Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36" 
   
   #Set RCurl parameters
@@ -47,12 +36,32 @@ login = function(username = character(),
   ScantronHandle = getCurlHandle()
   curlSetOpt(.opts = Options, curl = ScantronHandle)  
   
-  #Load the login page
-  x = getURI(url = 'https://admin.achievementseries.com/!/login.ssp', 
-             curl = ScantronHandle)
   
-  #Log In
-  x=postForm(loginurl, .params = pars, curl=ScantronHandle, .checkParams = FALSE)
+  
+  #Login step 0: Load the login page
+  x = getURI(url = loginurl1, curl = ScantronHandle)
+  
+  
+  
+  #Login step 1: Enter the siteID
+  Token = getToken(x)
+  pars=list("SiteID" = SiteCode,
+            "returnUrl" = "/Auth/Login/User",
+            "__RequestVerificationToken" = Token)
+  x = postForm(uri = loginurl1, .params = pars, curl=ScantronHandle, .checkParams = FALSE)
+  
+  
+  
+  #Log In step 2: enter the username and password
+  OrgID = getOrgID(x)
+  pars=list(
+    "Username" = username,
+    "Password" = password,
+    "returnUrl" = "/Auth/Login/User",
+    "__RequestVerificationToken" = Token,
+    "OrganizationId" = OrgID
+  )
+  x=postForm(uri = loginurl2, .params = pars, curl=ScantronHandle, .checkParams = FALSE)
   
   return(ScantronHandle)
 }
