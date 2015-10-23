6707f79418627805967282f4e5ee9b76084d90e4
pj.debarros@gmail.com <pj.debarros@gmail.com>
2015-10-23 08:54:59 -0400

    Created the ScoreUpdates function, located in the ScoreUpdates.R file.

---
+++ ScoreUpdates.R
@@ -0,0 +1,45 @@
+#Score Updates
+
+ScoreUpdates = function(x,y,cap){
+  #cap is the maximum number of scan events a test can have and still be an update
+  #x = RecentEventFrame
+  #y = RecentTestFrame
+  
+  #Convert the student names to character format
+  x$StNameRep = as.character(x$StNameRep)
+  
+  #Convert the test names to character format
+  x$Published.Test = as.character(x$Published.Test)
+  y$Published.Test = as.character(y$Published.Test)
+  
+  #Limit the set of tests to just those that have fewer than cap scans
+  y = y[y$Count < cap,]
+  
+  #Extract the numerical score
+  x$Score2 = substr(x$Score,1,regexpr(pattern = "\\(", text =  x$Score)-1)
+  
+  #Limit to just the tests and columns of interest
+  x =  x[x$Published.Test %in% y$Published.Test,c(2,5,8,1)]
+  
+  
+  
+  #Order by Name and Test
+  x = x[order(x$StNameRep),]
+  x = x[order(x$Published.Test),]
+  
+  #get rid of the rownames
+  rownames(x) = NULL
+  rownames(y) = NULL
+  
+  #Create the output
+  write.csv(x, file = "recentScores.csv")
+  
+}
+
+#The better way to do this would be to create something that takes each test,
+#and creates a character string to match what I would put in the email.
+#The character string (complete with spacing and carriage returns) 
+#could be written to a file.
+#In fact, all of them could be written to the same file, 
+#with some sort of vertical delimeter.
+#What function writes a text file?
