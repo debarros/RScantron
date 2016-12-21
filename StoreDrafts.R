8b4f9ac9186dae60c4f8f4ea4aa41787a46803f3
debarros <pj.debarros@gmail.com>
2016-12-21 09:03:01 -0500

    Created StoreDrafts.R - function StoreDrafts()

---
+++ StoreDrafts.R
@@ -0,0 +1,15 @@
+#StoreDrafts.R
+StoreDrafts = function(DraftFrame, MaxDrafts = NA_integer_){
+  urlStub = "https://admin.achievementseries.com/test-draft/content/list.ssp?id="
+  DraftFrame$page = ""
+  if(is.na(MaxDrafts)){
+    lastRow = nrow(DraftFrame)
+  } else { 
+    lastRow = MaxDrafts
+  }
+  
+  for(i in 1:lastRow){
+    DraftFrame$page[i] = getURI(paste0(urlStub,DraftFrame$tid[i]), curl=ScantronHandle)
+  }
+  return(DraftFrame)
+}
