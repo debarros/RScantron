a463f649cb67fd58e7da9cda919c96dc01e72a0c
debarros <pj.debarros@gmail.com>
2017-09-27 12:50:12 -0400

    moved some random stuff out of MasterScript.R and into Cataloging.R

---
+++ Cataloging.R
@@ -0,0 +1,26 @@
+#Cataloging.R
+
+# Catalog Draft Tests ####
+# Get the complete list of folders with their folder ID's
+DraftFolderFrame = FindFolders(ScantronHandle, "d", SkipDraftFolder)
+SessionFolderFrame = FindFolders(ScantronHandle, "s", SkipSessionFolder)
+
+
+# Get the complete list of test drafts with their test ID's and containing folders
+DraftFrame = FindDrafts(DraftFolderFrame)
+
+# Get the page showing the content of each draft
+# If you have a lot of drafts, include the parameter MaxDrafts = n (where n is some small integer)
+DraftFrame = StoreDrafts(DraftFrame)
+
+# Catalog class sections ####
+#Get the complete list of class sections with their class ID's
+ClassFrame = FindClasses(ScantronHandle)
+ClassFrame = ClassFrame[order(ClassFrame$Dept, ClassFrame$ShortName, ClassFrame$Level, ClassFrame$Primary.Staff),]
+write.csv(ClassFrame,file = "ClassSections.csv", row.names = FALSE)
+
+# Catalog all test scores ###
+#Compile all scores ever
+testIDs = TestFrame$tid
+AllResults = FindResults(testIDs, ScantronHandle)
+
