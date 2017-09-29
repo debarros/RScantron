d53350915331c6ce0dc2bb7e1969e6024add6d5d
debarros <pj.debarros@gmail.com>
2017-09-28 20:15:09 -0400

    FindRecent.R - changed the FindRecentEvents function to allow a google sheet url, worksheet name, and initial cell to be passed in so that new events can be compared with known events stored in the google sheet.  Also added a logical parameter "newScores" that determines whether new scores for existing events should be considered new events.  Also added a character vector parameter "status" which causes the result to be reduced to those rows whose status appears in the parameter.

--- FindRecent.R
+++ FindRecent.R
@@ -1,9 +1,24 @@
 #FindRecentEvents function
 
-FindRecentEvents = function(EventFrame, RecentDays){
-  RecentEventFrame = EventFrame[which(EventFrame$Date >= Sys.Date()-(RecentDays - 1)),]
-  RecentEventFrame = RecentEventFrame[RecentEventFrame$Status == "Finished", ]
-  return(RecentEventFrame)
+FindRecentEvents = function(EventFrame, RecentDays = NULL, url = NULL, ws = NULL, newScores = T, status = c("Finished"), startcell = "A1"){
+  if(!is.null(RecentDays)){
+    RecentEventFrame = EventFrame[which(EventFrame$Date >= Sys.Date()-(RecentDays - 1)),]
+    RecentEventFrame = RecentEventFrame[RecentEventFrame$Status == "Finished", ]
+    return(RecentEventFrame) 
+  } else if(is.null(url) | is.null(ws)){
+    stop("If RecentDays is not specified, then both url and ws must be.")
+  } else {
+    PriorEventFrame = gs_read(ss = url, ws = ws) #read in existing events
+    gs_edit_cells(ss = url, ws = ws, input = EventFrame, anchor = startcell) #store the complete events
+    IDcolumns = colnames(EventFrame)
+    if(!newScores){
+      IDcolumns = IDcolumns[!(IDcolumns %in% c("Score"))]
+    }
+    PriorEventFrame$identifier = apply(PriorEventFrame[IDcolumns], MARGIN = 1, FUN = paste0, collapse = "-")
+    EventFrame$identifier = apply(EventFrame[IDcolumns], MARGIN = 1, FUN = paste0, collapse = "-")
+    RecentEventFrame = EventFrame[!(EventFrame$identifier %in% PriorEventFrame$identifier),1:(ncol(EventFrame)-1)]
+    RecentEventFrame = RecentEventFrame[RecentEventFrame$Status %in% status, ]
+  }
 }
 
 
