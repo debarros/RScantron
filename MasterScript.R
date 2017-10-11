06c1cfadb32d0eedc440ab4718f05fc00a51f186
debarros <pj.debarros@gmail.com>
2017-10-11 14:22:49 -0400

    MasterScript.R - added commented out line for alternate way of pull the ReventEventFrame; replaced the missing tests section with a call to UpdateTab function; replaced the code to pull the item response files with a call to GetAndStoreItemResponses function.

--- MasterScript.R
+++ MasterScript.R
@@ -21,7 +21,6 @@ ScantronHandle = login(username, password, SiteCode, caLocation)
 gs_auth() # this might launch a browser so you can sign into your account
 ScannedTests.url = gs_url(ScannedTests.url.text) #enter the URL of the scanned test document here
 
-
 #-----------------------------------------#
 #### Determine current reporting needs ####
 #-----------------------------------------#
@@ -30,7 +29,8 @@ StudentFrame = FindStudents(ScantronHandle) # Get the complete list of students
 EventFrame = FindEvents(StudentFrame, ScantronHandle, schoolYear()) # Get the complete list of instances in which a student has taken a test
 
 # Compare new event frame to old event frame and subset to the recent events
-RecentEventFrame = FindRecentEvents(EventFrame = EventFrame, TAB = list(TAB.wb, TABpath), status = "Finished")
+RecentEventFrame = FindRecentEvents(EventFrame = EventFrame, TAB = list(TAB.wb, TABpath), status = "Finished", updatePriorEvents = T)
+# RecentEventFrame = FindRecentEvents(EventFrame = EventFrame, RecentDays = 5, status = "Finished", updatePriorEvents = F)
 
 #Get a list of the recently scanned tests, and how many instances per test
 RecentTestFrame = FindRecentTests(RecentEventFrame)
@@ -39,58 +39,18 @@ RecentTestFrame = FindRecentTests(RecentEventFrame)
 TestFolderFrame = FindFolders(ScantronHandle, "t", SkipTestFolder)
 TestFrame = FindTests(TestFolderFrame)
 
-
 # Check for tests not included in the tab
 missingTests = RecentTestFrame$Published.Test[!(RecentTestFrame$Published.Test %in% TAB$TestName)]
 print(missingTests)
 
-# If there are any missing tests, add them to the TAB
-if(length(missingTests) > 0){ 
-  TestFrame.temp = TestFrame
-  TestFrame$Local.folder = NA_character_
-  TestFrame = TestFrame[,c(3,1,2,4)]
-  colnames(TestFrame) = colnames(TAB)
-  NewTestFrame = rbind.data.frame(TAB, TestFrame)
-  NewTestFrame = NewTestFrame[!duplicated(NewTestFrame$TestName),]
-  NewTestFrame = NewTestFrame[order(NewTestFrame$TestName),]
-  for(i in 1:ncol(NewTestFrame)){
-    NewTestFrame[,i] = na.to.empty(NewTestFrame[,i])
-  }
-  writeData(wb = TAB.wb, sheet = "TAB", x = NewTestFrame)
-  saveWorkbook(wb = TAB.wb, file = "\\\\stuthin2/Data/tests/2017-2018/TAB.xlsx", overwrite = T)
-  TestFrame = TestFrame.temp
-} # /if there are missing tests
-
-#Reload the TAB
-TAB = readWorkbook(xlsxFile = TABpath, sheet = "TAB")
+# If there are any missing tests, add them to the TAB and reload it
+UpdateTab(missingTests, TestFrame, TAB, TAB.wb, TABpath)
+TAB = read.xlsx(xlsxFile = TABpath, sheet = "TAB")
 
 # Download the item response files and save them
-for(i in 1:nrow(RecentTestFrame)){
-  # Get the current test name, code, and id
-  testname = as.character(RecentTestFrame$Published.Test[i])
-  testcode = substr(testname, start = 1, stop = regexpr(pattern = " ", text = testname) - 1)
-  testid = TestFrame$tid[TestFrame$TestName == testname]
-  testpath = TAB$Local.folder[TAB$TestID == testid][1]
+GetAndStoreItemResponses(RecentTestFrame, TestFrame, TAB, ScantronHandle, Coursecode2Testcode, Coursecode2Course, Sections)
+# GetAndStoreItemResponses_SingleTest(testname = "Gv (2017-10-05) Constitution and Bill of Rights", TAB, Coursecode2Testcode, Coursecode2Course)
   
-  # determine the courses associated with this test code
-  coursecodes = colnames(Coursecode2Testcode)[Coursecode2Testcode[Coursecode2Testcode$testcode == testcode,] == 1]
-  courses = Coursecode2Course$Course[Coursecode2Course$CourseCode %in% coursecodes]
-  
-  # determine the sections of this course
-  currentSections = Sections[Sections$ClassName %in% courses,]
-  
-  for(j in 1:nrow(currentSections)){
-    currentClassID = currentSections$ClassID[j] # get the ClassID for the section
-    currentClassName = paste0(currentSections$TeacherName[j],"_p", currentSections$Period[j])
-    
-    # download the item response file
-    currentresponses = GetItemResponses(ClassID = currentClassID, TestID = testid, curlhandle = ScantronHandle)
-    
-    # store it in the exports folder
-    StoreItemResponses(responses = currentresponses, testpath = testpath, classname = currentClassName)
-  } #/for each section
-} #/for each reportable test
-
 # Log out of scantron
 LogoutPage = logout(ScantronHandle)
 
