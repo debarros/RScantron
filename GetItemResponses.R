c7cce1a4da4348da01aec7e9e54f9334ac49c091
debarros <pj.debarros@gmail.com>
2017-10-18 11:16:53 -0400

    GetItemResponses.R - implemented custom sectionining, broke out the code that determines current sections into its own function DetermineCurrentSections()

--- GetItemResponses.R
+++ GetItemResponses.R
@@ -10,21 +10,19 @@ GetAndStoreItemResponses = function(RecentTestFrame, TestFrame, TAB, ScantronHan
   }
   
   for(i in 1:nrow(RecentTestFrame)){
+    print(paste0("row ",i," of ",nrow(RecentTestFrame)))
     # Get the current test name, code, and id
     testname = as.character(RecentTestFrame$Published.Test[i])
     testcode = substr(testname, start = 1, stop = regexpr(pattern = " ", text = testname) - 1)
     testid = TestFrame$tid[TestFrame$TestName == testname]
     testpath = TAB$Local.folder[TAB$TestID == testid][1]
     
-    # determine the courses associated with this test code
-    coursecodes = colnames(Coursecode2Testcode)[Coursecode2Testcode[Coursecode2Testcode$testcode == testcode,] == 1]
-    courses = Coursecode2Course$Course[Coursecode2Course$CourseCode %in% coursecodes]
     
-    # determine the sections of this course
-    currentSections = Sections[Sections$ClassName %in% courses,]
-    classIDs = currentSections$ClassID
+    currentSections = DetermineCurrentSections(testname, CustomSectioning, Sections, testcode, Coursecode2Testcode, Coursecode2Course)
+
+    # Determine the class names and download the item response CSV's
     classnames = paste0(currentSections$TeacherName,"_p", currentSections$Period, currentSections$Level)
-    GetAndStoreItemResponses_1test(classIDs, classnames, testid, testpath, ScantronHandle)
+    GetAndStoreItemResponses_1test(currentSections$ClassID, classnames, testid, testpath, ScantronHandle)
     
   } #/for each reportable test
 }
@@ -41,22 +39,17 @@ GetAndStoreItemResponses_1test = function(classIDs, classnames, testid, testpath
     
     # store it in the exports folder
     StoreItemResponses(responses = currentresponses, testpath = testpath, classname = currentClassName)
-  } #/for each section
+  } # /for each section
 }
 
 
-GetAndStoreItemResponses_SingleTest = function(testname, TAB, Coursecode2Testcode, Coursecode2Course){
+GetAndStoreItemResponses_SingleTest = function(testname, TAB, Coursecode2Testcode, Coursecode2Course, CustomSectioning, Sections){
   testcode = substr(testname, start = 1, stop = regexpr(pattern = " ", text = testname) - 1)
   testid = TAB$TestID[TAB$TestName == testname][1]
   testpath = TAB$Local.folder[TAB$TestID == testid][1]
-  
-  # determine the courses associated with this test code
-  coursecodes = colnames(Coursecode2Testcode)[Coursecode2Testcode[Coursecode2Testcode$testcode == testcode,] == 1]
-  courses = Coursecode2Course$Course[Coursecode2Course$CourseCode %in% coursecodes]
-  currentSections = Sections[Sections$ClassName %in% courses,]
-  classIDs = currentSections$ClassID
+  currentSections = DetermineCurrentSections(testname, CustomSectioning, Sections, testcode, Coursecode2Testcode, Coursecode2Course)
   classnames = paste0(currentSections$TeacherName,"_p", currentSections$Period, currentSections$Level)
-  GetAndStoreItemResponses_1test(classIDs, classnames, testid, testpath, ScantronHandle)
+  GetAndStoreItemResponses_1test(currentSections$ClassID, classnames, testid, testpath, ScantronHandle)
 }
 
 
@@ -73,3 +66,25 @@ GetItemResponses_1section = function(ClassID, TestID, curlhandle){
   responses = gsub(pattern = "\r\n", replacement = "\n",x = responses) # replace CRLF with LF to avoid blank lines
   return(responses)
 } # /GetItemResponses
+
+
+
+
+
+DetermineCurrentSections = function(testname, CustomSectioning, Sections, testcode, Coursecode2Testcode, Coursecode2Course){
+  
+  # Determine the sections for this test code
+  if(testname %in% CustomSectioning$TestTitle){ # if there are custom sections, just get those
+    classIDs = CustomSectioning$ClassID_List[CustomSectioning$TestTitle == testname]
+    classIDs = strsplit(x = classIDs, split = ",")
+    currentSections = Sections[Sections$ClassID %in% classIDs,]
+  } else { # if there are no custom sections
+    # Determine the courses associated with this test code
+    coursecodes = colnames(Coursecode2Testcode)[Coursecode2Testcode[Coursecode2Testcode$testcode == testcode,] == 1]
+    courses = Coursecode2Course$Course[Coursecode2Course$CourseCode %in% coursecodes]
+    # Get all the sections of those courses
+    currentSections = Sections[Sections$ClassName %in% courses,]
+  } # /if-else to determine sections
+  
+  return(currentSections)
+}
