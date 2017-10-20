74d3a750cb3499cecda2e495fb6b7e3e1567a355
debarros <pj.debarros@gmail.com>
2017-10-20 08:57:18 -0400

    Fixed the DetermineCurrentSections function so that it accurately pulls the currentSections data.frame for tests with custom sectioning

--- GetItemResponses.R
+++ GetItemResponses.R
@@ -76,7 +76,7 @@ DetermineCurrentSections = function(testname, CustomSectioning, Sections, testco
   # Determine the sections for this test code
   if(testname %in% CustomSectioning$TestTitle){ # if there are custom sections, just get those
     classIDs = CustomSectioning$ClassID_List[CustomSectioning$TestTitle == testname]
-    classIDs = strsplit(x = classIDs, split = ",")
+    classIDs = strsplit(x = classIDs, split = ",")[[1]] # strsplit returns a list, but we just want the contents of the list, hence the [[1]]
     currentSections = Sections[Sections$ClassID %in% classIDs,]
   } else { # if there are no custom sections
     # Determine the courses associated with this test code
