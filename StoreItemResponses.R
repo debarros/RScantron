3b7ee698a277b6c75ef0e9dd3b2e7efbb35c4ac7
debarros <pj.debarros@gmail.com>
2017-10-31 09:15:51 -0400

    In StoreItemResponses function, added code to create the exports folder if it does not already exist.

--- StoreItemResponses.R
+++ StoreItemResponses.R
@@ -1,7 +1,9 @@
 #Store Item Responses
 
 StoreItemResponses = function(responses, testpath, classname, datafolder = "exports"){
-  classname = gsub(pattern = "/", replacement = "+", x = classname)
-  filename = paste0(testpath, "/", datafolder, "/", classname, "_itemresponses.csv")
-  cat(responses, file = filename)
-}
+  datapath = paste0(testpath, "/", datafolder)                      # Build the path to the datafolder
+  classname = gsub(pattern = "/", replacement = "+", x = classname) # Replace forward slashes with plusses in the class name
+  filename = paste0(datapath, "/", classname, "_itemresponses.csv") # Build the filename
+  if(!dir.exists(datapath)){dir.create(datapath)}                   # If the datafolder doesn't exist, create it
+  cat(responses, file = filename)                                   # Save the file to the datafolder
+} # /function
