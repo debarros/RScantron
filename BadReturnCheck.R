20893aed14c689c9691bb08e66ba3fe095eafaf4
debarros <pj.debarros@gmail.com>
2017-12-11 14:19:49 -0500

    Created BadReturnCheck function that checks pages downloaded from the website to see if they are what gets returned when the user is not logged in

---
+++ BadReturnCheck.R
@@ -0,0 +1,9 @@
+# BadReturnCheck.R
+page = x
+BadReturnCheck = function(page, messageLevel = 0){
+  badReturnCheck = grep(pattern = "Admin Site, Enter Your Username/Password", x = page, ignore.case = T)
+  badReturnCheck = badReturnCheck + grep(pattern = "position: absolute", x = page, ignore.case = T)
+  badReturnCheck = badReturnCheck + grep(pattern = "header_loggedout_container", x = page, ignore.case = T)
+  badReturnCheck = identical(badReturnCheck, as.integer(3))
+  return(badReturnCheck)
+}
