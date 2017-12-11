f8b8b5621af841e4fd520c307dc79855a3796917
debarros <pj.debarros@gmail.com>
2017-12-11 14:25:37 -0500

    logout.R - fuxed some formatting, added some comments, added a call to BadReturnCheck()

--- logout.R
+++ logout.R
@@ -5,15 +5,18 @@
 
 logout = function(ScantronHandle, messageLevel = 0){
   #First, get the home page
-  x = getURI(url = 'https://admin.achievementseries.com/home/home.ssp', 
-             curl = ScantronHandle)
+  x = getURI(url = 'https://admin.achievementseries.com/home/home.ssp', curl = ScantronHandle)
+  
+  if(BadReturnCheck(x)){
+    stop("You are already logged out.")
+  } 
   
   #Find the location of the logout link
-  y = gregexpr("Logout\\?z=.",x)[[1]][1]
+  y = gregexpr("Logout\\?z=.", x)[[1]][1]
   
   #Assemble the URI for the logout link
-  z = paste0("https://admin.achievementseries.com",substring(x,y-6,y+15))
+  z = paste0("https://admin.achievementseries.com", substring(x, y - 6, y + 15))
   
   #Fetch the URI, causing the session to be terminated
   getURI(url = z, curl = ScantronHandle)
-}
+} # /function
