b53ff0e241e723d92693b8785b47d98b9519341b
debarros <pj.debarros@gmail.com>
2017-12-11 14:22:23 -0500

    FindTests.R - messageLevel stuff

--- FindTests.R
+++ FindTests.R
@@ -3,10 +3,13 @@
 #This is a wrapper function for FindTests_1Folder() to make it run once for each folder in TestFolderFrame
 
 FindTests = function (TestFolderFrame, messageLevel = 0){
+  
+  if(messageLevel > 0){ print("Finding tests.")}
+  
   TestFrame = data.frame(stringsAsFactors = FALSE)
   for (i in 1:nrow(TestFolderFrame)){
     TestFolderRow = TestFolderFrame[i,]
-    x = FindTests_1Folder(TestFolderRow)
+    x = FindTests_1Folder(TestFolderRow, messageLevel - 1)
     TestFrame = rbind(TestFrame, x)
   }
   return(TestFrame)
