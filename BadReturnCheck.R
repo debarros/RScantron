2dba1992c17649c016b1e9360f85a6ebcade7cc0
abthornejr <abthornejr@gmail.com>
2017-12-12 16:04:20 -0500

    Commenting out an errant line from some sandbox testing.

--- BadReturnCheck.R
+++ BadReturnCheck.R
@@ -1,5 +1,5 @@
 # BadReturnCheck.R
-page = x
+# page = x
 BadReturnCheck = function(page, messageLevel = 0){
   badReturnCheck = grep(pattern = "Admin Site, Enter Your Username/Password", x = page, ignore.case = T)
   badReturnCheck = badReturnCheck + grep(pattern = "position: absolute", x = page, ignore.case = T)
