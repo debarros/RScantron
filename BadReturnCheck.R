26bb2e42aa9f2b27fe9a01d7b0fa006f65b53988
debarros <pj.debarros@gmail.com>
2018-02-12 20:08:26 -0500

    Modified BadReturnCheck function to work if the user hasn't even entered a site id yet.

--- BadReturnCheck.R
+++ BadReturnCheck.R
@@ -1,9 +1,15 @@
 # BadReturnCheck.R
 # page = x
+
+# Given a page pulled from the Scantron website, this function determines whether you are logged out
+# If you are logged out, it returns TRUE.  Otherwise, FALSE.
 BadReturnCheck = function(page, messageLevel = 0){
   badReturnCheck = grep(pattern = "Admin Site, Enter Your Username/Password", x = page, ignore.case = T)
+  if(length(badReturnCheck) == 0){
+    badReturnCheck = badReturnCheck + grep(pattern = "Admin Site, Enter Your Site ID", x = page, ignore.case = T)
+  }
   badReturnCheck = badReturnCheck + grep(pattern = "position: absolute", x = page, ignore.case = T)
   badReturnCheck = badReturnCheck + grep(pattern = "header_loggedout_container", x = page, ignore.case = T)
   badReturnCheck = identical(badReturnCheck, as.integer(3))
   return(badReturnCheck)
-}
+} #/BadReturnCheck function
