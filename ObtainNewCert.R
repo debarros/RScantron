3f3d1b9282219e52d89137685eb6321db190cc5c
debarros <pj.debarros@gmail.com>
2018-02-16 12:24:41 -0500

    ObtainNewCert - added messageLevel print statement, moved the download.file command to inside the conditional statement so that it only downloads if no cert file is specified

--- ObtainNewCert.R
+++ ObtainNewCert.R
@@ -5,13 +5,18 @@
 # Otherwise, it sets the folder path and file name to the default
 
 ObtainNewCert = function(caLocation, messageLevel = 0){
+  
+  if(messageLevel > 0){
+    print("Checking for certification file")
+  }
+  
   if(length(caLocation) == 0){
     caLocation = "cacert.pem.crt"
-  } 
-  download.file(url = "http://curl.haxx.se/ca/cacert.pem", 
-                destfile = caLocation, 
-                method = "auto", 
-                quiet = TRUE, 
-                mode = "w")    
+    download.file(url = "http://curl.haxx.se/ca/cacert.pem", 
+                  destfile = caLocation, 
+                  method = "auto", 
+                  quiet = TRUE, 
+                  mode = "w")
+  }
   return(caLocation)
 }
