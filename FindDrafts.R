e0620bd7526ae3d443910fa5d9ce28a774dd6a16
debarros <pj.debarros@gmail.com>
2018-02-16 12:07:01 -0500

    FindDrafts - added messageLevel print statement

--- FindDrafts.R
+++ FindDrafts.R
@@ -1,11 +1,18 @@
 # FindDrafts.R
 
 FindDrafts = function (DraftFolderFrame, messageLevel = 0){
+  
+  if(messageLevel > 0){
+    print("Finding test drafts")
+  }
+  
   DraftFrame = data.frame(stringsAsFactors = FALSE)
   for (i in 1:nrow(DraftFolderFrame)){
     DraftFolderRow = DraftFolderFrame[i,]
     x = FindDrafts_1Folder(DraftFolderRow)
     DraftFrame = rbind(DraftFrame, x)
   } # /for
+  
   return(DraftFrame)
+  
 } # /function
