51f64c94a2199109014eca991cdf212e5006f3ce
debarros <pj.debarros@gmail.com>
2018-02-16 12:06:42 -0500

    FindClasses - moved default agent value to within the function, reformatted some code, adjusted some comments

--- FindClasses.R
+++ FindClasses.R
@@ -1,20 +1,25 @@
 #FindClasses.R
 
-FindClasses = function(agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
-                       messageLevel = 0){
+FindClasses = function(agent = NULL, messageLevel = 0){
+  
+  if(is.null(agent)){
+    agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
+  }
+  
+  if(messageLevel > 0){
+    print("Finding classes")
+  }
   
   #Set the URL's to be used
-  url1 = "https://admin.achievementseries.com/classes/list.ssp"                #set the url for the class list page
-  url2 = "https://admin.achievementseries.com/classes/list.csv?_list=Classes"  #set the url for the csv of classes
+  url1 = "https://admin.achievementseries.com/classes/list.ssp"                # set the url for the class list page
+  url2 = "https://admin.achievementseries.com/classes/list.csv?_list=Classes"  # set the url for the csv of classes
   
-  #Pull the classes page in order to get the ClassID from the links
-  x <-                        #fetch the page with the list of classes
-    httr::content(
-      httr::GET(url = url1,
-                user_agent(agent)),
-      as = "text",
-      encoding = "UTF-8"
-    )
+  # Pull the classes page in order to get the ClassID from the links
+  # fetch the page with the list of classes
+  x <- httr::content(
+    httr::GET(url = url1, user_agent(agent)),
+    as = "text",
+    encoding = "UTF-8")
   
   
   # Check to make sure it worked
@@ -22,33 +27,32 @@ FindClasses = function(agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWe
     stop("Error!  You are no longer logged in to Scantron.  Log in and then run this command again.")
   }
   
-  page = htmlParse(x)                                             #convert the page to a parsed html object
-  links = xpathSApply(page, "//a/@href")                          #this finds all of the links in the document
-  ClassLinks = substr(links[grep("info.ssp\\?id=",links)],20,35)  #this pulls out the ClassID's from the links
+  page = htmlParse(x)                                             # convert the page to a parsed html object
+  links = xpathSApply(page, "//a/@href")                          # this finds all of the links in the document
+  ClassLinks = substr(links[grep("info.ssp\\?id=",links)],20,35)  # this pulls out the ClassID's from the links
   
-  #Pull the csv with the rest of the class info
-  x <-                #pull the csv of classes
-    httr::content(
-      httr::GET(url = url2,
-                user_agent(agent)),
-      as = "text",
-      encoding = "UTF-8"
-    )
-  ClassFrame = read.csv(textConnection(x), stringsAsFactors = FALSE) #treat the page as a csv and make a data.frame
-  ClassFrame$ClassID = ClassLinks                                    #add the ClassID's to the dataframe of classes
+  # Pull the csv with the rest of the class info
+  # pull the csv of classes
+  x <- httr::content(
+    httr::GET(url = url2, user_agent(agent)),
+    as = "text",
+    encoding = "UTF-8")
   
-  #Find important locations in the character strings holding the class names, section numbers, and period numbers
-  m = unlist(regexec(pattern = "Sec. " ,text = ClassFrame$Class))  #location of section number
-  p = unlist(regexec(pattern = "," ,text = ClassFrame$Class))      #location of comma ending the section number
-  r = unlist(regexec(pattern = "Block " ,text = ClassFrame$Class)) #location of period number
-  n = nchar(ClassFrame$Class)                                      #location end of period number (last character)
+  ClassFrame = read.csv(textConnection(x), stringsAsFactors = FALSE) # treat the page as a csv and make a data.frame
+  ClassFrame$ClassID = ClassLinks                                    # add the ClassID's to the dataframe of classes
   
-  #Add columns to ClassFrame to hold the section, period, and class name
+  # Find important locations in the character strings holding the class names, section numbers, and period numbers
+  m = unlist(regexec(pattern = "Sec. " ,text = ClassFrame$Class))  # location of section number
+  p = unlist(regexec(pattern = "," ,text = ClassFrame$Class))      # location of comma ending the section number
+  r = unlist(regexec(pattern = "Block " ,text = ClassFrame$Class)) # location of period number
+  n = nchar(ClassFrame$Class)                                      # location end of period number (last character)
+  
+  # Add columns to ClassFrame to hold the section, period, and class name
   ClassFrame$Section = substr(ClassFrame$Class, m+5, p-1)
   ClassFrame$Period = substr(ClassFrame$Class, r+6, n)
   ClassFrame$ClassName = substr(ClassFrame$Class, 1, unlist(m)-1)
   
-  #Use the class and teacher lookup tables to add the abbreviated teacher name, short course name, and academic department
+  # Use the class and teacher lookup tables to add the abbreviated teacher name, short course name, and academic department
   ClassFrame$TeacherName = TeacherLookup$TeacherName[match(x = ClassFrame$Primary.Staff, table = TeacherLookup$LastFirst)]
   ClassFrame$Dept = CourseLookup$Dept[match(x = ClassFrame$ClassName, table = CourseLookup$Course.Name)]
   ClassFrame$ShortName = CourseLookup$ShortName[match(x = ClassFrame$ClassName, table = CourseLookup$Course.Name)]
@@ -56,4 +60,4 @@ FindClasses = function(agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWe
   ClassFrame$Level[is.na(ClassFrame$Level)] = ""
   
   return(ClassFrame)
-} #end of FindClasses() function
+} # end of FindClasses() function
