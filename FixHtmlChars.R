273d017e6b9429801e72ff9beec5207eeb9f349e
debarros <pj.debarros@gmail.com>
2018-02-16 12:22:53 -0500

    FixHtmlChars - added messageLevel print statement, added newline at end of function

--- FixHtmlChars.R
+++ FixHtmlChars.R
@@ -1,6 +1,9 @@
 # FixHtmlChars.R
 
 FixHtmlChars = function(x, messageLevel = 0){
+  if(messageLevel > 0){
+    print("Fixing weird html characters")
+  }
   x = gsub("&gt;", ">", x)  # switch from html code for > to just the symbol
   x = gsub("&amp;", "&", x) # switch from html code for & to just the symbol
   return(x)
