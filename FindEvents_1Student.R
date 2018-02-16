04e712b060fae6f3e41c6d5b53c5fc19a50f35e3
debarros <pj.debarros@gmail.com>
2018-02-16 12:09:04 -0500

    FindEvents_1Student - moved default agent value to inside of function, added and reformatted some comments

--- FindEvents_1Student.R
+++ FindEvents_1Student.R
@@ -1,19 +1,18 @@
 #FindEvents_1Student.R
 
-FindEvents_1Student = function(sid,
-                               attempt = 1,
-                               messageLevel = 0,
-                               agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36") {
-  tests <- httr::content(httr::GET(
-    url = paste0(
-      'https://admin.achievementseries.com/student/test-results.csv?id=',
-      sid,
-      '&_list=TestResults'
-    ),
-    user_agent(agent)
-  ),
-  as = "text",
-  encoding = "UTF-8")
+FindEvents_1Student = function(sid, attempt = 1, messageLevel = 0, agent = NULL) {
+  
+  if(is.null(agent)){
+    agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
+  }
+  
+  tests <- httr::content(
+    httr::GET(
+      url = paste0('https://admin.achievementseries.com/student/test-results.csv?id=',sid,'&_list=TestResults'),
+      user_agent(agent)),
+    as = "text",
+    encoding = "UTF-8")
+  
   # tests <- httr::content(tests, as = "text")
   
   write(tests, file = "~/testpageoutput.html") # This next section is for debugging when an nrow(x) error pops up
@@ -22,10 +21,10 @@ FindEvents_1Student = function(sid,
     print(paste0("attempt ", attempt))
   }
   
-  #Convert the page to a usable R object
-  q = read.csv(textConnection(tests), stringsAsFactors = FALSE) #treat the page as a csv and make a data.frame
+  # Convert the page to a usable R object
+  q = read.csv(textConnection(tests), stringsAsFactors = FALSE) # treat the page as a csv and make a data.frame
   
-  #Recursive call: If the http request got an error page, redo the request
+  # Recursive call: If the http request got an error page, redo the request
   if (ncol(q) != 7) {
     if (messageLevel > 0) {
       print("e1")
@@ -34,12 +33,11 @@ FindEvents_1Student = function(sid,
       sid = sid,
       attempt = attempt + 1,
       messageLevel = messageLevel,
-      agent = agent
-    )
+      agent = agent)
     if (messageLevel > 0) {
       print("e2")
     }
-  }
+  } # /if ncol(q) != 7
   
   if (messageLevel > 0) {
     print("h")
