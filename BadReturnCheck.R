f729d3fcc1d0729297ae34f9d121ad776695bf38
debarros <pj.debarros@gmail.com>
2018-02-16 12:05:28 -0500

    BadReturnCheck - added messageLevel print statement

--- BadReturnCheck.R
+++ BadReturnCheck.R
@@ -4,6 +4,11 @@
 # Given a page pulled from the Scantron website, this function determines whether you are logged out
 # If you are logged out, it returns TRUE.  Otherwise, FALSE.
 BadReturnCheck = function(page, messageLevel = 0){
+  
+  if(messageLevel > 0){
+    print("Checking to see if you are logged in.")
+  }
+  
   badReturnCheck = grep(pattern = "Admin Site, Enter Your Username/Password", x = page, ignore.case = T)
   if(length(badReturnCheck) == 0){
     badReturnCheck = badReturnCheck + grep(pattern = "Admin Site, Enter Your Site ID", x = page, ignore.case = T)
