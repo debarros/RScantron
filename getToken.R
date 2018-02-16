6b608f8126fe212d6560315a189d940527453787
debarros <pj.debarros@gmail.com>
2018-02-16 12:26:35 -0500

    getToken - added messageLevel print statement, reformatted some comments

--- getToken.R
+++ getToken.R
@@ -1,10 +1,14 @@
 #getToken.R
 
-# This function takes one argument, x.  It is the initial login page.
+# This function takes the following arguments:
+#   x - the initial login page
 # It finds and return the post form value "__RequestVerificationToken"
 # This is necessary (I think) for maintaining a consistent identity during the login process
 
 getToken = function(x, messageLevel = 0){
+  if(messageLevel > 0){
+    print("getting the request verification token")
+  }
   TokenInputStart = regexpr(pattern = "__RequestVerificationToken", text = x)[1] #Find where the input tag starts
   ValueLocations = gregexpr(pattern = 'value=', text = x)[[1]]                   #Find where the "value"s are
   TokenStart = ValueLocations[ValueLocations > TokenInputStart][1] + 7           #Find the end of the first "value" after the start of the tag
