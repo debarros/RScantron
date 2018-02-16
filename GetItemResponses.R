dc430432e27200de4c24753dd05c58169c47c9bf
debarros <pj.debarros@gmail.com>
2018-02-16 12:23:47 -0500

    GetItemResponses - reformatted some code for readability,

--- GetItemResponses.R
+++ GetItemResponses.R
@@ -1,20 +1,14 @@
-#Get Item Responses
+# GetItemResponses.R
 
 #--------------------------------#
 #### GetAndStoreItemResponses ####
 #--------------------------------#
-GetAndStoreItemResponses = function(RecentTestFrame,
-                                    TestFrame,
-                                    TAB.wb,
-                                    messageLevel = 0,
-                                    agent = NULL) {
+GetAndStoreItemResponses = function(RecentTestFrame, TestFrame, TAB.wb, messageLevel = 0, agent = NULL) {
   if(is.null(agent)){
     agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
   }
   
-  Coursecode2Testcode = read.xlsx(xlsxFile = TAB.wb,
-                                  sheet = "Course Codes",
-                                  startRow = 2)
+  Coursecode2Testcode = read.xlsx(xlsxFile = TAB.wb, sheet = "Course Codes", startRow = 2)
   Coursecode2Course =  set_colnames(
     x = as.data.frame(t(
       read.xlsx(
@@ -33,11 +27,9 @@ GetAndStoreItemResponses = function(RecentTestFrame,
   
   # Check for testcodes missing from the TAB
   testnames = as.character(RecentTestFrame$Published.Test)
-  testcodes = unique(substr(
-    testnames,
-    start = 1,
-    stop = regexpr(pattern = " ", text = testnames) - 1
-  ))
+  testcodes = unique(substr(testnames,
+                            start = 1,
+                            stop = regexpr(pattern = " ", text = testnames) - 1))
   testcodes = testcodes[!(testcodes %in% Coursecode2Testcode$testcode)]
   if (length(testcodes) > 0) {
     stop(
@@ -63,33 +55,15 @@ GetAndStoreItemResponses = function(RecentTestFrame,
     
     testpath = TAB$Local.folder[TAB$TestID == testid][1]
     
-    currentSections = DetermineCurrentSections(
-      testname,
-      CustomSectioning,
-      Sections,
-      testcode,
-      Coursecode2Testcode,
-      Coursecode2Course
-    )
+    currentSections = DetermineCurrentSections(testname, CustomSectioning, Sections, testcode, Coursecode2Testcode, Coursecode2Course)
     
     # Determine the class names and download the item response CSV's
-    classnames = paste0(
-      currentSections$TeacherName,
-      "_p",
-      currentSections$Period,
-      currentSections$Level
-    )
-    GetAndStoreItemResponses_1test(
-      classIDs = currentSections$ClassID,
-      classnames,
-      testid,
-      testpath,
-      messageLevel = messageLevel - 1,
-      agent = agent
-    )
+    classnames = paste0(currentSections$TeacherName, "_p", currentSections$Period, currentSections$Level)
+    GetAndStoreItemResponses_1test(classIDs = currentSections$ClassID, classnames, testid, 
+                                   testpath, messageLevel = messageLevel - 1, agent = agent)
     
   } # /for each reportable test
-} # /GetAndStoreItemResponses
+} # /GetAndStoreItemResponses function
 
 
 
@@ -99,12 +73,7 @@ GetAndStoreItemResponses = function(RecentTestFrame,
 #--------------------------------------#
 #### GetAndStoreItemResponses_1test ####
 #--------------------------------------#
-GetAndStoreItemResponses_1test = function(classIDs,
-                                          classnames,
-                                          testid,
-                                          testpath,
-                                          messageLevel = 0,
-                                          agent = NULL) {
+GetAndStoreItemResponses_1test = function(classIDs, classnames, testid, testpath, messageLevel = 0, agent = NULL) {
   if(is.null(agent)){
     agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
   }
@@ -182,21 +151,13 @@ GetItemResponses_1section = function(ClassID, TestID, messageLevel = 0, agent = 
   if(is.null(agent)){
     agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
   }
+  
+  url = paste0("https://admin.achievementseries.com/report/class/responses.csv?c=", ClassID, "&t=", TestID, "&v=table&_list=Students")
   responses <- httr::content(
-    httr::GET(
-      url = paste0(
-        "https://admin.achievementseries.com/report/class/responses.csv?",
-        "c=",
-        ClassID,
-        "&t=",
-        TestID,
-        "&v=table&_list=Students"),
-      user_agent(agent)),
+    httr::GET(url = url, user_agent(agent)),
     as = "text",
     encoding = "UTF-8")
   # responses = httr::content(x, as = "text")
-  responses = gsub(pattern = "\r\n",
-                   replacement = "\n",
-                   x = responses) # replace CRLF with LF to avoid blank lines
+  responses = gsub(pattern = "\r\n", replacement = "\n", x = responses) # replace CRLF with LF to avoid blank lines
   return(responses)
 } # /GetItemResponses_1section
