9897ba024277519592ef7c47db05302301283976
debarros <pj.debarros@gmail.com>
2018-02-16 12:12:37 -0500

    FindRecent - added messageLevel print statements, reformatted comments,

--- FindRecent.R
+++ FindRecent.R
@@ -1,30 +1,36 @@
-#FindRecentEvents function
+# FindRecent.R
 
+# FindRecentEvents function
 FindRecentEvents = function(EventFrame, RecentDays = NULL, url.ws = NULL, TAB = NULL, newScores = T, 
                             status = c("Finished"), startcell = "A1", updatePriorEvents = T, messageLevel = 0){
   
   if(!is.null(RecentDays)){
-    RecentEventFrame = EventFrame[which(EventFrame$Date >= Sys.Date()-(RecentDays - 1)),]
-    RecentEventFrame = RecentEventFrame[RecentEventFrame$Status == "Finished", ]
+    if(messageLevel > 0){
+      print(paste0("Finding events from the last ", RecentDays, " days."))
+    }
+    RecentEventFrame = EventFrame[which(EventFrame$Date >= Sys.Date() - (RecentDays - 1)),]
+    RecentEventFrame = RecentEventFrame[RecentEventFrame$Status == "Finished",]
     return(RecentEventFrame) 
     
   } else if(!is.null(url.ws)){
     if(length(url.ws) != 2){
       stop("url.ws must be a list of length 2, where the first element is the result of gs_url() and the second is a character value")
     }
-    PriorEventFrame = gs_read(ss = url.ws[[1]], ws = url.ws[[2]]) #read in existing events
-    gs_edit_cells(ss = url, ws = ws, input = EventFrame, anchor = startcell) #store the complete events
+    PriorEventFrame = gs_read(ss = url.ws[[1]], ws = url.ws[[2]])                    # read in existing events
+    gs_edit_cells(ss = url, ws = ws, input = EventFrame, anchor = startcell)         # store the complete events
     return(FindRecentEvents.compare(EventFrame, newScores, PriorEventFrame, status))
     
   } else if(!is.null(TAB)){
     if(length(TAB) != 2){
-      stop("TAB must be a list of length 2, where the first element is an openxlsx workbook object and the second is a character value with the workbook's file path")
+      stop(paste0("TAB must be a list of length 2, ",
+                  "where the first element is an openxlsx workbook object", 
+                  "and the second is a character value with the workbook's file path"))
     }
     TAB.wb = TAB[[1]]
     TABpath = TAB[[2]]
     PriorEventFrame = read.xlsx(xlsxFile = TABpath, sheet = "Events")          # read in existing events
     PriorEventFrame$Date = as.Date(PriorEventFrame$Date, origin = "1899-12-30")
-    if(updatePriorEvents){                                                     # If the table of prior events is to be updated in the TAB, do so.
+    if(updatePriorEvents){                                                     # update prior event table in TAB (if necessary)
       writeData(wb = TAB.wb, sheet = "Events", x = EventFrame)                 # store the complete events
       saveWorkbook(wb = TAB.wb, file = TABpath, overwrite = T)
     }
@@ -36,7 +42,15 @@ FindRecentEvents = function(EventFrame, RecentDays = NULL, url.ws = NULL, TAB = 
 } # /FindRecentEvents function
 
 
+
+
+# FindRecentEvents.compare function
 FindRecentEvents.compare = function(EventFrame, newScores, PriorEventFrame, status, messageLevel = 0){
+  
+  if(messageLevel > 0){
+    print("Comparing event frame to remove events that have already been reported")
+  }
+  
   IDcolumns = colnames(EventFrame)
   if(!newScores){
     IDcolumns = IDcolumns[!(IDcolumns %in% c("Score"))]
@@ -45,18 +59,25 @@ FindRecentEvents.compare = function(EventFrame, newScores, PriorEventFrame, stat
   EventFrame$identifier = apply(EventFrame[IDcolumns], MARGIN = 1, FUN = paste0, collapse = "-")
   RecentEventFrame = EventFrame[!(EventFrame$identifier %in% PriorEventFrame$identifier),1:(ncol(EventFrame)-1)]
   RecentEventFrame = RecentEventFrame[RecentEventFrame$Status %in% status, ]
+  
   return(RecentEventFrame) 
+  
 } # /FindRecentEvents.compare function
 
 
 
 
-#FindRecentTests function
+# FindRecentTests function
 FindRecentTests = function(RecentEventFrame, messageLevel = 0){
   if(nrow(RecentEventFrame) == 0){
     print("There are no recent tests.")
     return(NULL)
   }
+  
+  if(messageLevel > 0){
+    print("Finding recently scanned tests")
+  }
+  
   RecentTestFrame = data.frame(Published.Test = unique(RecentEventFrame$Published.Test))
   RecentTestFrame$Date = as.Date(NA)
   for (i in 1:nrow(RecentTestFrame)){
