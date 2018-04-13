86d941a373ff8abb239f5a37c862947643deb96c
debarros <pj.debarros@gmail.com>
2018-04-13 12:23:53 -0400

    Fixed a line in BadReturnCheck to prevent getting NA's

--- BadReturnCheck.R
+++ BadReturnCheck.R
@@ -11,7 +11,7 @@ BadReturnCheck = function(page, messageLevel = 0){
   
   badReturnCheck = grep(pattern = "Admin Site, Enter Your Username/Password", x = page, ignore.case = T)
   if(length(badReturnCheck) == 0){
-    badReturnCheck = badReturnCheck + grep(pattern = "Admin Site, Enter Your Site ID", x = page, ignore.case = T)
+    badReturnCheck = grep(pattern = "Admin Site, Enter Your Site ID", x = page, ignore.case = T)
   }
   badReturnCheck = badReturnCheck + grep(pattern = "position: absolute", x = page, ignore.case = T)
   badReturnCheck = badReturnCheck + grep(pattern = "header_loggedout_container", x = page, ignore.case = T)
