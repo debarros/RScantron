178c28e68962b454a49427797ba62561297b9ec6
debarros <pj.debarros@gmail.com>
2018-06-12 14:54:01 -0400

    Spoil.R - added roxygen2 comments to the functions; created a variable to hold the url stub for spoiling an event; added before and after parameters to the Spoil_1Test function

--- Spoil.R
+++ Spoil.R
@@ -1,10 +1,17 @@
 # Spoil.R
 
+#' @title Spoil 1 Event
+#' @description Spoil a given testing event
+#' @param Event a one row data frame with the format of the output from FindRecentTests
+#' @param messageLevel integer of length 1 indicating level of diagnostic messages to print
+#' @return This function does not return anything
 Spoil_1Event = function(Event, messageLevel = 0, agent = NULL){
   
   if(is.null(agent)){
     agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
   }
+  
+  SpoilPage = "https://admin.achievementseries.com/test-instance/spoil.ssp"
   
   # Get the events page for the relevant student
   studentEventsPage <- httr::content(
@@ -33,7 +40,7 @@ Spoil_1Event = function(Event, messageLevel = 0, agent = NULL){
   # Click the spoil link
   spoilPage1 <- httr::content(
     httr::GET(
-      url = paste0('https://admin.achievementseries.com/test-instance/spoil.ssp?id=',EventID),
+      url = paste0(SpoilPage,'?id=',EventID),
       user_agent(agent)),
     as = "text",
     encoding = "UTF-8")
@@ -67,7 +74,7 @@ Spoil_1Event = function(Event, messageLevel = 0, agent = NULL){
               "_PageName"        = "1",
               "Confirmation"     = "T",
               "_PageAction"      = "n")
-  spoilPage2 <- httr::POST(url = "https://admin.achievementseries.com/test-instance/spoil.ssp", 
+  spoilPage2 <- httr::POST(url = SpoilPage, 
                            user_agent(agent), 
                            body = pars, 
                            encode = "multipart")
@@ -76,7 +83,7 @@ Spoil_1Event = function(Event, messageLevel = 0, agent = NULL){
   pars = list("_hasCancelButton" = "yes",
               "_PageName"        = "2",
               "_PageAction"      = "c")
-  spoilPage2 <- httr::POST(url = "https://admin.achievementseries.com/test-instance/spoil.ssp", 
+  spoilPage2 <- httr::POST(url = SpoilPage, 
                            user_agent(agent), 
                            body = pars, 
                            encode = "multipart")
@@ -89,7 +96,11 @@ Spoil_1Event = function(Event, messageLevel = 0, agent = NULL){
 
 
 
-
+#' @title Spoil
+#' @description Spoil a set of testing events
+#' @param SpoilFrame a data frame with the format of the output from FindRecentTests
+#' @param messageLevel integer of length 1 indicating level of diagnostic messages to print
+#' @return This function does not return anything
 Spoil = function(SpoilFrame, messageLevel = 0, agent = NULL){
   
   if(is.null(agent)){
@@ -111,14 +122,33 @@ Spoil = function(SpoilFrame, messageLevel = 0, agent = NULL){
 
 
 
-
-Spoil_1Test = function(EventFrame, PublishedTest, messageLevel = 0, agent = NULL){
+#' @title Spoil 1 Test
+#' @description Spoil all events for a given test
+#' @param EventFrame the output from FindEvents
+#' @param PublishedTest the name of the published test whose events are to be spoiled
+#' @param before date or character coercible to date.  Only events before 'before' will be spoiled
+#' @param after date or character coercible to date.  Only events after 'after' will be spoiled
+#' @param messageLevel integer of length 1 indicating level of diagnostic messages to print
+#' @return This function does not return anything
+Spoil_1Test = function(EventFrame, PublishedTest, before = NULL, after = NULL, messageLevel = 0, agent = NULL){
+  
+  if(!is.null(before)){ before = as.Date(before) }
+  if(!is.null(after)){  after  = as.Date(after)  }
+  
+  if(!(is.null(before) | is.null(after))){
+    if(after > before){
+      stop("ERROR!  after cannot be a later date than before")
+    }
+  }
   
   if(is.null(agent)){
     agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
   }
   
   SpoilFrame = EventFrame[EventFrame$Published.Test == PublishedTest,]
+  
+  if(!is.null(before)){ SpoilFrame = SpoilFrame[SpoilFrame$Date < before,] }
+  if(!is.null(after)){  SpoilFrame = SpoilFrame[SpoilFrame$Date > after,]  }
   
   Spoil(SpoilFrame, messageLevel = messageLevel - 1, agent = agent)
   
@@ -130,7 +160,12 @@ Spoil_1Test = function(EventFrame, PublishedTest, messageLevel = 0, agent = NULL
 
 
 
-
+#' @title Spoil 1 Student
+#' @description Spoil all events for a given student
+#' @param EventFrame the output from FindEvents
+#' @param StudentNumber the ID number of the student whose testing events are to be spoiled
+#' @param messageLevel integer of length 1 indicating level of diagnostic messages to print
+#' @return This function does not return anything
 Spoil_1Student = function(EventFrame, StudentNumber, messageLevel = 0, agent = NULL){
   
   if(is.null(agent)){
