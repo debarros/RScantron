cad7c33f04fe9bd41414092a296769c71906969d
debarros <pj.debarros@gmail.com>
2018-06-15 14:23:26 -0400

    FindRecent.R - added roxygen comments to FindRecentEvents, FindRecentEvents.compare, and FindRecentTests.  Also reformatted some comments and code.  Also changed FindRecentEvents and FindRecentTests functions to have only one return statement.

--- FindRecent.R
+++ FindRecent.R
@@ -1,16 +1,32 @@
 # FindRecent.R
 
-# FindRecentEvents function
+#' @title Find Recent Events
+#' @description Identify recent testing events
+#' @param EventFrame output from FindEvents function
+#' @param RecentDays how many days to look back.
+#' @param url.ws list of length 2, where the first element is the result of
+#'   gs_url() and the second is a character value.
+#' @param TAB list of length 2. The first element is an openxlsx workbook object
+#'   created by running loadworkbook on the TAB file.  The second is a character
+#'   value with the TAB workbook's file path.
+#' @param newScores logical.  Should new scores be considered new testing events.
+#' @param status character vector of statuses to include.  Defaults to "Finished".
+#' @param startcell
+#' @param updatePriorEvents logical - should the table of prior events be
+#'   updates with new recent events.
+#' @param messageLevel integer of length 1 indicating level of diagnostic
+#'   messages to print.  Defaults to 0.
+#' @return data.frame with one row for each recent testing event
+#' @details At least one of \code{RecentDays}, \code{url.ws}, or \code{TAB} must
+#'   be provided.  If \code{TAB} or \code{url.ws} is provided but
+#'   \code{RecentDays} is not, the function FindRecentEvents.compare is called.
 FindRecentEvents = function(EventFrame, RecentDays = NULL, url.ws = NULL, TAB = NULL, newScores = T, 
                             status = c("Finished"), startcell = "A1", updatePriorEvents = T, messageLevel = 0){
   
   if(!is.null(RecentDays)){
-    if(messageLevel > 0){
-      print(paste0("Finding events from the last ", RecentDays, " days."))
-    }
-    RecentEventFrame = EventFrame[which(EventFrame$Date >= Sys.Date() - (RecentDays - 1)),]
-    RecentEventFrame = RecentEventFrame[RecentEventFrame$Status == "Finished",]
-    return(RecentEventFrame) 
+    if(messageLevel > 0){ print(paste0("Finding events from the last ", RecentDays, " days.")) }
+    RecentEventFrame = EventFrame[EventFrame$Date >= Sys.Date() - (RecentDays - 1),]
+    RecentEventFrame = RecentEventFrame[RecentEventFrame$Status  %in% status,]
     
   } else if(!is.null(url.ws)){
     if(length(url.ws) != 2){
@@ -18,7 +34,7 @@ FindRecentEvents = function(EventFrame, RecentDays = NULL, url.ws = NULL, TAB = 
     }
     PriorEventFrame = gs_read(ss = url.ws[[1]], ws = url.ws[[2]])                    # read in existing events
     gs_edit_cells(ss = url, ws = ws, input = EventFrame, anchor = startcell)         # store the complete events
-    return(FindRecentEvents.compare(EventFrame, newScores, PriorEventFrame, status))
+    RecentEventFrame = FindRecentEvents.compare(EventFrame, newScores, PriorEventFrame, status)
     
   } else if(!is.null(TAB)){
     if(length(TAB) != 2){
@@ -34,22 +50,35 @@ FindRecentEvents = function(EventFrame, RecentDays = NULL, url.ws = NULL, TAB = 
       writeData(wb = TAB.wb, sheet = "Events", x = EventFrame)                 # store the complete events
       saveWorkbook(wb = TAB.wb, file = TABpath, overwrite = T)
     }
-    return(FindRecentEvents.compare(EventFrame, newScores, PriorEventFrame, status, messageLevel = messageLevel - 1))
+    RecentEventFrame = FindRecentEvents.compare(EventFrame, newScores, PriorEventFrame, status, messageLevel = messageLevel - 1)
     
   } else {
-    stop("You must specify at least one of RecentDays, url.ws, or TABpath")
+    stop("You must specify at least one of RecentDays, url.ws, or TAB")
   }
+  
+  return(RecentEventFrame)
 } # /FindRecentEvents function
 
 
 
 
-# FindRecentEvents.compare function
+#' @title Find Recent Events By Comparison
+#' @description Identify recent testing events using a table of prior testing
+#'   events.
+#' @param EventFrame output from FindEvents function.
+#' @param newScores logical.  Should new scores be considered new testing
+#'   events.
+#' @param PriorEventFrame data.frame with a table of prior events read from the
+#'   TAB or a google sheet.
+#' @param status character vector of statuses to include.  Defaults to
+#'   "Finished".
+#' @param messageLevel integer of length 1 indicating level of diagnostic
+#'   messages to print.  Defaults to 0.
+#' @return data.frame with one row for each recent testing event
+#' @details This function is called from the FindRecentEvents function.
 FindRecentEvents.compare = function(EventFrame, newScores, PriorEventFrame, status, messageLevel = 0){
   
-  if(messageLevel > 0){
-    print("Comparing event frame to remove events that have already been reported")
-  }
+  if(messageLevel > 0){ print("Comparing event frame to remove events that have already been reported") }
   
   IDcolumns = colnames(EventFrame)
   if(!newScores){
@@ -61,31 +90,35 @@ FindRecentEvents.compare = function(EventFrame, newScores, PriorEventFrame, stat
   RecentEventFrame = RecentEventFrame[RecentEventFrame$Status %in% status, ]
   
   return(RecentEventFrame) 
-  
 } # /FindRecentEvents.compare function
 
 
 
 
-# FindRecentTests function
+#' @title Find Recent Tests
+#' @description Check the data.frame of recent testing events and compile info
+#'   on recent scanned tests.
+#' @param RecentEventFrame output from the FindRecentEvents function.
+#' @param messageLevel integer of length 1 indicating level of diagnostic
+#'   messages to print.  Defaults to 0.
+#' @return data.frame with one row for each recently scanned test
 FindRecentTests = function(RecentEventFrame, messageLevel = 0){
   if(nrow(RecentEventFrame) == 0){
     print("There are no recent tests.")
-    return(NULL)
-  }
+    RecentTestFrame = NULL
+  } else {
+    
+    if(messageLevel > 0){ print("Finding recently scanned tests") }
+    
+    RecentTestFrame = data.frame(Published.Test = unique(RecentEventFrame$Published.Test))
+    RecentTestFrame$Date = as.Date(NA)
+    for (i in 1:nrow(RecentTestFrame)){
+      RecentTestFrame$Count[i] = sum(RecentEventFrame$Published.Test == RecentTestFrame$Published.Test[i])
+      RecentTestFrame$Date[i] = as.Date(min(
+        RecentEventFrame$Date[RecentEventFrame$Published.Test == RecentTestFrame$Published.Test[i]]))
+    } #/for
+    RecentTestFrame = RecentTestFrame[order(-RecentTestFrame$Count), ]
+  } #/if-else
   
-  if(messageLevel > 0){
-    print("Finding recently scanned tests")
-  }
-  
-  RecentTestFrame = data.frame(Published.Test = unique(RecentEventFrame$Published.Test))
-  RecentTestFrame$Date = as.Date(NA)
-  for (i in 1:nrow(RecentTestFrame)){
-    RecentTestFrame$Count[i] = sum(RecentEventFrame$Published.Test == RecentTestFrame$Published.Test[i])
-    RecentTestFrame$Date[i] = as.Date(min(
-      RecentEventFrame$Date[which(
-        RecentEventFrame$Published.Test == RecentTestFrame$Published.Test[i])]))
-  }
-  RecentTestFrame = RecentTestFrame[order(-RecentTestFrame$Count), ]
   return(RecentTestFrame)
 } # /FindRecentTests function
