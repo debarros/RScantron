26ac9c9aa49c0e1f574bd759a3754d31fce6da06
debarros <pj.debarros@gmail.com>
2018-10-25 07:28:05 -0400

    In StoreItemResponses.R, added message printing and error handling

--- StoreItemResponses.R
+++ StoreItemResponses.R
@@ -10,9 +10,19 @@
 #' @return Nothing gets returned by this function
 #' @details This takes an R object holding the table of item responses for given section on a given test and save it as a csv
 StoreItemResponses = function(responses, testpath, classname, datafolder = "exports", messageLevel = 0){
+  
+  if (messageLevel > 0) {
+    print(paste0("    storing ", classname, " in ", testpath))
+  } # /if messageLevel > 0
+  
+  if(!file.exists(testpath)){
+    stop(paste0("The local folder path for this test (according to the TAB) does not exist. \n", testpath))
+  }
+  
   datapath = paste0(testpath, "/", datafolder)                      # Build the path to the datafolder
   classname = gsub(pattern = "/", replacement = "+", x = classname) # Replace forward slashes with plusses in the class name
   filename = paste0(datapath, "/", classname, "_itemresponses.csv") # Build the filename
   if(!dir.exists(datapath)){dir.create(datapath)}                   # If the datafolder doesn't exist, create it
   cat(responses, file = filename)                                   # Save the file to the datafolder
+  
 } # /function
