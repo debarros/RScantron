2d1f9bfb1244fcfb48a1e81601ff2716a284fe3e
debarros <pj.debarros@gmail.com>
2019-11-22 14:10:50 -0500

    UpdateTab.R - changed the way it sorts so things are grouped by folder in the output; added lots of comments

--- UpdateTab.R
+++ UpdateTab.R
@@ -13,18 +13,17 @@ UpdateTab = function(missingTests, TestFrame, TAB.wb, TABpath, messageLevel = 0)
   if(messageLevel > 0){
     print("Updating the TAB")
   }
-  TAB = read.xlsx(xlsxFile = TAB.wb, sheet = "TAB")
-  if(length(missingTests) > 0){ 
-    TestFrame.temp = TestFrame
-    TestFrame$Local.folder = NA_character_
-    TestFrame = TestFrame[,c(3,1,2,4)]
-    colnames(TestFrame) = colnames(TAB)
-    NewTestFrame = rbind.data.frame(TAB, TestFrame)
-    NewTestFrame = NewTestFrame[!duplicated(NewTestFrame$TestName),]
-    NewTestFrame = NewTestFrame[order(NewTestFrame$TestName),]
-    NewTestFrame = DFna.to.empty(NewTestFrame)                       # Convert NA's to empty cells
-    NewTestFrame = NewTestFrame[order(NewTestFrame$Local.folder == "", decreasing = T),]
-    writeData(wb = TAB.wb, sheet = "TAB", x = NewTestFrame)
-    saveWorkbook(wb = TAB.wb, file = TABpath, overwrite = T)
+  TAB = read.xlsx(xlsxFile = TAB.wb, sheet = "TAB")                                      # Grab the TAB sheet of the TAB
+  if(length(missingTests) > 0){                                                          # If there are any missing tests,
+    TestFrame.temp = TestFrame                                                           # Make a copy of the TestFrame (for some reason?)
+    TestFrame$Local.folder = NA_character_                                               # Add a Local.Folder column
+    TestFrame = TestFrame[,c(3,1,2,4)]                                                   # Rearrange the columns
+    colnames(TestFrame) = colnames(TAB)                                                  # Make the column names match
+    NewTestFrame = rbind.data.frame(TAB, TestFrame)                                      # Merge the two sets of tests
+    NewTestFrame = NewTestFrame[order(NewTestFrame$Local.folder, na.last = F),]          # Arrange by folder location and put the ones with missing folders first
+    NewTestFrame = NewTestFrame[!duplicated(NewTestFrame$TestName),]                     # Remove duplicate entries
+    NewTestFrame = DFna.to.empty(NewTestFrame)                                           # Convert NA's to empty cells
+    writeData(wb = TAB.wb, sheet = "TAB", x = NewTestFrame)                              # Put the data into the workbook object
+    saveWorkbook(wb = TAB.wb, file = TABpath, overwrite = T)                             # Save the workbook to the file system
   } # /if there are missing tests
 } # /function
