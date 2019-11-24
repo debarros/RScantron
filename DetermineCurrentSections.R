d1923311896989c76df8d6020ff51c3bad7f044d
debarros <pj.debarros@gmail.com>
2019-11-23 21:27:41 -0500

    DetermineCurrentSections.R - formatted some comments and some code; added some comments; added error handling for when there are courses found but no sections of those courses

--- DetermineCurrentSections.R
+++ DetermineCurrentSections.R
@@ -19,20 +19,31 @@ DetermineCurrentSections = function(testname, CustomSectioning,
   if(messageLevel > 0){ print(paste0("Determining sections for test code ", testcode)) }
   
   # Determine the sections for this test code
-  if(testname %in% CustomSectioning$TestTitle){ # if there are custom sections, just get those
+  if(testname %in% CustomSectioning$TestTitle){                                       # if there are custom sections, just get those
     classIDs = CustomSectioning$ClassID_List[CustomSectioning$TestTitle == testname]
-    classIDs = strsplit(x = classIDs, split = ",")[[1]] # strsplit returns a list, but we just want the contents of the list, hence the [[1]]
-    if(all(classIDs %in% Sections$ClassID)){
+    classIDs = strsplit(x = classIDs, split = ",")[[1]]                               # strsplit returns a list, but we just want the contents of the list, hence the [[1]]
+    if(all(classIDs %in% Sections$ClassID)){                                          # Make sure the classIDs specified in the custom sectioning exist
       currentSections = Sections[Sections$ClassID %in% classIDs,]  
     } else {
       badIDs = classIDs[!(classIDs %in% Sections$ClassID)]
       stop(paste0("The following classIDs are associated with ", testname, ", but are not in the Sections table in the TAB: ",VectorSentence(badIDs)))
     }
-  } else { # if there are no custom sections
-    # Determine the courses associated with this test code
-    courses = DetermineCourses(testcode, Coursecode2Testcode, Coursecode2Course, messageLevel = messageLevel - 1)
+  } else {                                                                            # if there are no custom sections
+    
+    courses = DetermineCourses(                                                       # Determine the courses associated with this test code
+      testcode = testcode, 
+      Coursecode2Testcode = Coursecode2Testcode, 
+      Coursecode2Course = Coursecode2Course, 
+      messageLevel = messageLevel - 1)
+    
     # Get all the sections of those courses
     currentSections = Sections[Sections$ClassName %in% courses,]
+    
+    if(nrow(currentSections) < 1){
+      stop(paste0("Although the testcode ", testcode, "is associated with the course(s) ", VectorSentence(courses), ", 
+                  there are no sections for those courses in the section table of the TAB."))
+    }
+    
   } # /if-else to determine sections
   
   if(messageLevel > 0){ print(paste0("Done determining sections for this test code")) }
@@ -59,5 +70,8 @@ DetermineCourses = function(testcode,
                             messageLevel = 0){
   coursecodes = colnames(Coursecode2Testcode)[Coursecode2Testcode[Coursecode2Testcode$testcode == testcode,] == 1]
   courses = Coursecode2Course$Course[Coursecode2Course$CourseCode %in% coursecodes]
+  if(length(courses) < 1){
+    stop(paste0("The testcode ", testcode, " is not associated with any courses."))
+  }
   return(courses)
 } #/ DetermineCourses function
